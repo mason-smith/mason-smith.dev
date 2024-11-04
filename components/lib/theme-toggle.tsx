63b0dbd108b32c03b82bac2d4c5a134c91110a63
Mason Smith <mason.smith@solutionops.com>
2024-11-04 06:17:20 -0500

    feat: change theme toggle from dropdown to button

--- components/lib/theme-toggle.tsx
+++ components/lib/theme-toggle.tsx
@@ -1,20 +1,16 @@
-"use client";
+'use client';
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
 
 // local dependencies
-import { Button } from "../ui/button";
+import { Button } from '../ui/button';
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
 
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    >
+    <Button variant="ghost" size="icon" onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
       <SunIcon className="h-[1.5rem] w-[1.3rem] dark:hidden" />
       <MoonIcon className="hidden h-5 w-5 dark:block" />
       <span className="sr-only">Toggle theme</span>
