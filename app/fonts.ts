2061fd05f02699556b839fbfacf41d8500e84910
Mason Smith <mason.smith@solutionops.com>
2024-11-07 07:18:24 -0500

    feat: add fonts file

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { GeistMono } from 'geist/font/mono';
+// import { Inter as FontSans } from 'next/font/google';
+import { GeistSans } from 'geist/font/sans';
+
+export const fontSans = GeistSans;
+// export const fontSans = FontSans({
+//   subsets: ['latin'],
+//   variable: '--font-sans',
+// });
+
+export const fontMono = GeistMono;
