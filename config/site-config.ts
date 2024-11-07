6abf83f33079067ae5bbba6bea6965a4850a1d67
Mason Smith <mason.smith@solutionops.com>
2024-11-07 07:19:02 -0500

    feat: create siteConfig file

---
+++ config/site-config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  baseUrl: "https://stack-fitness.vercel.app",
+  name: "Mason Smith",
+  description: "Mason Smith's personal website",
+};
+
+export type SiteConfig = typeof siteConfig;
