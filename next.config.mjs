55d2d14b17f0b8037063d4359ec26422ec719c3a
Mason Smith <mason.smith@solutionops.com>
2024-11-08 10:33:57 -0500

    feat: configure mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
+import createMDX from "@next/mdx";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import remarkGfm from "remark-gfm";
+import rehypeStarryNight from "rehype-starry-night";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
@@ -13,6 +19,14 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter, remarkGfm],
+    rehypePlugins: [rehypeStarryNight],
+  },
+});
+
+export default withMDX(nextConfig);
