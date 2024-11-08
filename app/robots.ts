90e318d414cdb5c22e957213329ab69ff087f976
Mason Smith <mason.smith@solutionops.com>
2024-11-07 19:21:59 -0500

    feat: add robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { siteConfig } from "@/config/site-config";
+
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+      },
+    ],
+    sitemap: `${siteConfig.baseUrl}/sitemap.xml`,
+  };
+}
