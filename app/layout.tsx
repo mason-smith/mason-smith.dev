73e65671a34516f38ef8091a8f9b43686ade7e9d
Mason Smith <mason.smith@solutionops.com>
2024-11-08 10:38:28 -0500

    feat: update main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,54 @@
-import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 
-import { SiteFooter } from '@/components/lib/site-footer';
-import { SiteHeader } from '@/components/lib/site-header';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+import { Footer } from '@/components/footer';
+import { Navbar } from '@/components/navbar';
 // project dependencies
-import { ThemeProvider } from '@/components/lib/theme-provider';
+import { ThemeProvider } from '@/components/theme-provider';
 // local dependencies
 import './css/reset.css';
 import './css/global.css';
+import { siteConfig } from '@/config/site-config';
 import { cn } from '@/utils/cn';
+import { fontMono, fontSans } from './fonts';
 
-export const metadata = {
-  title: 'Home | Mason Smith',
-  description: "Mason Smith's personal website",
-  author: 'Mason Smith',
-  creator: 'Mason Smith',
-  keywords: ['Mason Smith', 'Software', 'Engineer', 'Frontend'],
+export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.baseUrl),
+  title: {
+    default: `${siteConfig.name}`,
+    template: '%s | Personal Website',
+  },
+  description: `${siteConfig.name}'s personal website`,
+  creator: siteConfig.name,
+  keywords: [
+    siteConfig.name,
+    'Software',
+    'Engineer',
+    'Frontend',
+    'Politics',
+    'Leftist',
+    'Ideology',
+    'Leadership',
+    'Management',
+    'Culture',
+  ],
   openGraph: {
-    title: 'Mason Smith - Personal Website',
-    description: "Mason Smith's personal website",
-    siteName: 'Mason Smith',
+    title: `${siteConfig.name}'s personal website`,
+    description: `${siteConfig.name}'s personal website`,
+    url: siteConfig.baseUrl,
+    siteName: `${siteConfig.name}'s personal website`,
     locale: 'en_US',
     type: 'website',
   },
   robots: {
     index: true,
     follow: true,
-    nocache: true,
     googleBot: {
       index: true,
       follow: true,
-      noimageindex: true,
       'max-video-preview': -1,
       'max-image-preview': 'large',
       'max-snippet': -1,
@@ -44,25 +62,22 @@ export const viewport = {
   maximumScale: 1,
 };
 
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-});
-
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html
       lang="en"
-      className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}
+      className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable, fontMono.variable)}
       suppressHydrationWarning
     >
-      <body>
+      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <div className="relative flex min-h-screen flex-col">
-            <SiteHeader />
-            <div className="flex-1">{children}</div>
-            <SiteFooter />
-          </div>
+          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            <Navbar />
+            {children}
+            <Footer />
+            <Analytics />
+            <SpeedInsights />
+          </main>
         </ThemeProvider>
       </body>
     </html>
