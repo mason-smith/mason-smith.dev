f4ea11c40e9edee01d9194ae2d5cfb5f276aab62
Mason Smith <mason.smith@solutionops.com>
2024-11-08 10:48:41 -0500

    feat: update e2e specs

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -1,18 +1,25 @@
-import test, { expect } from '@playwright/test';
+import test, { expect } from "@playwright/test";
 
-test.describe('Main app flow', () => {
-  test('should have title', async ({ baseURL, page }) => {
-    page.goto(baseURL ?? (process.env['BASE_URL'] || 'http://localhost:3000'));
+test.describe("Main app flow", () => {
+  test("should have title", async ({ baseURL, page }) => {
+    page.goto(baseURL ?? (process.env["BASE_URL"] || "http://localhost:3000"));
 
-    await expect(page.getByRole('heading', { name: 'Mason Smith' })).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Mason Smith" })
+    ).toBeVisible();
 
-    await page.getByRole('link', { name: 'View posts' }).click();
-    await expect(page.getByRole('heading', { name: 'Posts and musings' })).toBeVisible();
+    await page.getByRole("link", { name: "blog" }).click();
 
-    await page.getByRole('link', { name: 'More details about me' }).click();
-    await expect(page.getByRole('heading', { name: 'About me' })).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Things I think about" })
+    ).toBeVisible();
 
-    await page.getByRole('link', { name: 'Home page' }).click();
-    await expect(page.getByRole('heading', { name: 'Mason Smith' })).toBeVisible();
+    await page.getByRole("link", { name: "about" }).click();
+    await expect(page.getByRole("heading", { name: "About me" })).toBeVisible();
+
+    await page.getByRole("link", { name: "home" }).click();
+    await expect(
+      page.getByRole("heading", { name: "Mason Smith" })
+    ).toBeVisible();
   });
 });
