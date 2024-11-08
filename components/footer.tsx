24db4053e5836a18ddd8a74cfbe013d61c79a7bd
Mason Smith <mason.smith@solutionops.com>
2024-11-07 19:43:33 -0500

    feat: move components out of lib folder

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 // import { HeaderFooterContent } from "./header-footer-content";
 
-import { ThemeToggle } from './theme-toggle';
+import { ThemeToggle } from "./theme-toggle";
 
 // export function Footer() {
 //   return (
@@ -43,14 +43,22 @@ export function Footer() {
           <ThemeToggle />
         </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">© {new Date().getFullYear()} MIT Licensed</p>
+      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+        © {new Date().getFullYear()} MIT Licensed
+      </p>
     </footer>
   );
 }
 
 function ArrowIcon() {
   return (
-    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <path
         d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
         fill="currentColor"
